42bc5666ab18691afac545bdd178b6225b5e9dd1
Jiatong Li <li.jiato@husky.neu.edu>
2019-12-23 00:03:27 -0800

    update feature

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          i was changed!
+          i was changed on feature branch!
         </p>
         <a
           className="App-link"
